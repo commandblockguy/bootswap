36465603572c419a0e791b09554ba121e901077f
commandblockguy <commandblockguy1@gmail.com>
2020-05-25 16:47:41 -0500

    Fix CRC for 5.1.5.0015

--- src/versions.c
+++ src/versions.c
@@ -5,7 +5,7 @@
 const struct version versions[NUM_VERSIONS] = {
     {{5,0,0,82}, {(void*)0x58FC, NULL,          NULL, (void*)0x5CAB},  1339699,  2079573},
     {{5,0,0,89}, {(void*)0x5907, NULL,          NULL, (void*)0x5CB6},   910250,  5941963},
-    {{5,1,5,14}, {(void*)0x5CC8, (void*)0x60DE, NULL, (void*)0x6077}, 16288302, 10641119},
+    {{5,1,5,14}, {(void*)0x5CC8, (void*)0x60DE, NULL, (void*)0x6077}, 16288302, 12835005},
     {{5,3,1,50}, {(void*)0x5ECF, NULL, (void*)0x647B, (void*)0x6401},  1825178, 10083338},
     {{5,3,6,17}, {(void*)0x62C0, NULL, (void*)0x686C, (void*)0x67F2},  4315312, 10417503},
 };
